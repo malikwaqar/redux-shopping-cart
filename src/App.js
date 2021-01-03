a3b00016332723bfefd0394852fea184552e0d6b
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2021-01-03 05:09:30 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+// initial Store
+const initialStore = {
+  cart: cartItems,
+  total: 10,
+  amount: 7,
+};
+
+const store = createStore(reducer, initialStore);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
